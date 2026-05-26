fff31fc7d62650c429dbb1510b8ebaaf853339a1
Kamil Mustafayev <github-email-kamil.mstfyv@gmail.com>
2026-05-26 12:05:12 +0400

    Create components and update courseinfo

---
+++ part1/courseinfo/src/Content.jsx
@@ -0,0 +1,18 @@
+const Content = (props) => {
+  const { part1, exercises1, part2, exercises2, part3, exercises3 } = props;
+  return (
+    <>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </>
+  );
+};
+
+export default Content;
